e12dc485c3d50a34dd29cccd484f8afd94440aaa
lakshmimurali <vmanikandan@zohocorp.com>
2024-02-12 22:12:41 +0530

    UI is coming.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ export default function App() {
       <Footer />
       <UpdateSelectedText />
       <RenderUIActionsBasedOnTextSelectionChange />
-      <DisplayResponseToUserAction />
     </div>
   );
 }
