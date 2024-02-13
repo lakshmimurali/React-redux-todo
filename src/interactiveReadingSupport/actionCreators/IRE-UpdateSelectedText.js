d387815c26207bbe04e55912a8177f6b2cdc6ff2
lakshmimurali <vmanikandan@zohocorp.com>
2024-02-13 13:40:57 +0530

    1 file modified

--- src/interactiveReadingSupport/actionCreators/IRE-UpdateSelectedText.js
+++ src/interactiveReadingSupport/actionCreators/IRE-UpdateSelectedText.js
@@ -1,4 +1,5 @@
 const actionCreatorForUpdatingSelectedText = (text = '') => {
+  console.log('inside actionCreatorForUpdatingSelectedText', text);
   return {
     type: 'update-selected-text',
     text: text,
