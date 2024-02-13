c79be1875f8d543048659be91ca0af7269e0220c
lakshmimurali <vmanikandan@zohocorp.com>
2024-02-13 12:56:16 +0530

    1 file modified

--- src/interactiveReadingSupport/components/Dictionary.js
+++ src/interactiveReadingSupport/components/Dictionary.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 
 function ShowMeaningForWord(props) {
   let [meaningForSelectedText, setMeaning] = useState('Loading...');
+
   let meaningInfoExists = props.meaningInfo !== undefined;
+
   console.log(meaningInfoExists, props.meaningInfo, props.selectedText);
+
   if (meaningInfoExists) {
     setMeaning(props.meaningInfo.meaning);
   } else {
