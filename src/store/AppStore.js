59cf4a642b3ed2c3305e7016d4efad862bf6e8c6
lakshmimurali <vmanikandan@zohocorp.com>
2024-02-13 11:54:23 +0530

    1 file modified

--- src/store/AppStore.js
+++ src/store/AppStore.js
@@ -1,11 +1,11 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import { applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 const initialState = { selectedView: 'All', toDos: [] };
 // sample state = { selectedView:"All|Active|Completed" , toDos:[{1:{id:1,task:a, completed:false}}, {2:{ id:2,task:b,completed:true}}]}
 
-const appStore = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const appStore = createStore(rootReducer, applyMiddleware(thunk));
 
 export default appStore;
