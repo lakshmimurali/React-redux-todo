dc06ddb137d42e36f718d5ee8026c708f79e8c25
lakshmimurali <vmanikandan@zohocorp.com>
2024-02-13 18:30:59 +0530

    1 file modified

--- src/interactiveReadingSupport/components/Dictionary.js
+++ src/interactiveReadingSupport/components/Dictionary.js
@@ -10,7 +10,7 @@ function ShowMeaningForWord(props) {
     props.selectedText
   );*/
 
-  console.log('In Dictionary Component');
+  console.log('In Dictionary Component', props.meaning);
 
   if (props.meaning !== undefined) {
     //console.log('props.meaningInfo.meaning', props.meaningInfo.meaning);
