7e9af482bf558341a9958e98786807a915cb8881
lakshmimurali <vmanikandan@zohocorp.com>
2024-02-13 08:21:41 +0530

    1 file modified

--- src/interactiveReadingSupport/components/Dictionary.js
+++ src/interactiveReadingSupport/components/Dictionary.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 
 function ShowMeaningForWord(props) {
   let [fetchData, setFetchData] = useState(false);
-  let meaningForSelectedText = props.meaningObj.meaning;
-  if (meaningForSelectedText !== 'Not Available') {
-    setFetchData(true);
-  } else {
-    props.serverAction(meaningForSelectedText);
+  let meaningInfoExists = props.meaningObj !== undefined;
+  if (meaningInfoExists) {
+    let meaningForSelectedText = props.meaningObj.meaning;
+    if (meaningForSelectedText !== 'Not Available') {
+      setFetchData(true);
+    } else {
+      props.serverAction(meaningForSelectedText);
+    }
   }
 
   return (
