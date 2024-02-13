cf879b8ceb5fc4b7f48cf98a8077a379a0834594
lakshmimurali <vmanikandan@zohocorp.com>
2024-02-13 14:17:50 +0530

    1 file modified

--- src/interactiveReadingSupport/containers/ContainerToUpdatedSelectedText.js
+++ src/interactiveReadingSupport/containers/ContainerToUpdatedSelectedText.js
@@ -10,18 +10,21 @@ function UpdateSelectedText({ dispatch }) {
   useEffect(() => {
     const handleSelection = (event) => {
       //let selectedWord = getSelectionText();
-      let selectedWord = window.getSelection().toString();
-      console.log(
-        'In  UpdateSelectedText container useeffect for mouseup event',
-        selectedWord
-      );
-      console.log(
-        'event.target IS >>>>>>>>>',
-        event.target.value,
-        event.target
-      );
+      if (event.target.nodeName === 'INPUT') {
+        let selectedWord = window.getSelection().toString();
+        console.log(
+          'In  UpdateSelectedText container useeffect for mouseup event',
+          selectedWord
+        );
+        console.log(
+          'event.target IS >>>>>>>>>',
+          event.target.value,
+          event.target,
+          event.target.nodeName
+        );
 
-      dispatch(actionCreatorForUpdatingSelectedText(selectedWord));
+        dispatch(actionCreatorForUpdatingSelectedText(selectedWord));
+      }
     };
     document.addEventListener('mouseup', handleSelection, true);
 
