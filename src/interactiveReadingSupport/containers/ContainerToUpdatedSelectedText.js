fdf2ad15a05ca093e177f81642e011d2796de76b
lakshmimurali <vmanikandan@zohocorp.com>
2024-02-15 16:27:41 +0530

    Unwanted Code Removed.

--- src/interactiveReadingSupport/containers/ContainerToUpdatedSelectedText.js
+++ src/interactiveReadingSupport/containers/ContainerToUpdatedSelectedText.js
@@ -1,23 +1,20 @@
 import React, { useEffect } from 'react';
-
 import { connect } from 'react-redux';
 
 import actionCreatorForUpdatingSelectedText from '../actionCreators/IRE-UpdateSelectedText.js';
 
-import getSelectionText from './helperToGetSelectedText.js';
-
 function UpdateSelectedText({ dispatch }) {
   useEffect(() => {
     const handleSelection = (event) => {
-      if (event.target.nodeName === 'INPUT') {
-        let selectedWord = window.getSelection().toString();
-        console.log(
-          'In  UpdateSelectedText container useeffect for selectionchange event',
-          selectedWord
-        );
+      console.log('selection event triggers', event.target);
 
-        dispatch(actionCreatorForUpdatingSelectedText(selectedWord));
-      }
+      let selectedWord = window.getSelection().toString();
+      console.log(
+        'In  UpdateSelectedText container useeffect for selectionchange event',
+        selectedWord
+      );
+
+      dispatch(actionCreatorForUpdatingSelectedText(selectedWord));
     };
     document.addEventListener('selectionchange', handleSelection, true);
 
