aad9fa7a0bf9312abe885e080a60e9f5f02d97f1
lakshmimurali <vmanikandan@zohocorp.com>
2024-03-07 18:44:24 +0530

    Authentication Added For App.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,22 @@ import appStore from './store/AppStore.js';
 
 import App from './App.js';
 import LoginForm from './LoginForm.js';
+import LoginFormPhoneEmail from './LoginForm_Phone.Email.js';
 
 function RenderApp() {
   const [loggedIn, setLoggedInState] = useState(false);
   return (
     <Provider store={appStore}>
-      {loggedIn === true ? <App /> : <LoginForm setValue={setLoggedInState} />}
+      {loggedIn === true ? (
+        <App />
+      ) : (
+        <LoginFormPhoneEmail setValue={setLoggedInState} />
+      )}
     </Provider>
   );
 }
 
 const rootElement = document.getElementById('root');
 render(<RenderApp />, rootElement);
+
+/*{loggedIn === true ? <App /> : <LoginForm setValue={setLoggedInState} />}*/
