887fcf5f1e1ff85efb944acbf5f148a13ed9e840
lakshmimurali <vmanikandan@zohocorp.com>
2024-03-21 14:32:39 +0530

    To Do Uniquness added.

--- src/actions/addToDo.js
+++ src/actions/addToDo.js
@@ -1,10 +1,10 @@
-let index = 0;
+//let index = 0;
 function constructAddToDoPayLoad(toDoValue) {
   console.log('From AddtoDoForm', toDoValue);
   return {
     type: 'ADD_TO_DO',
     value: toDoValue,
-    id: index++,
+    id: toDoValue,
   };
 }
 
