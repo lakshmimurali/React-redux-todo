1d20217db2b321212cd73629e3095dcda599abd5
lakshmimurali <vmanikandan@zohocorp.com>
2024-03-22 17:37:26 +0530

    Add fix to show tool buttons if the same text is selected multiple times

--- src/interactiveReadingSupport/reducers/IRE-SelectedTextReducer.js
+++ src/interactiveReadingSupport/reducers/IRE-SelectedTextReducer.js
@@ -5,7 +5,15 @@ const selectedTextReducer = (
   action
 ) => {
   let textValue = action.text || state;
+  console.log('inside  selectedTextReducer', textValue,state );
+  if(textValue === state)
+  {
+    console.log('Inside .... ')
+    return textValue+" ";
+  }
+  else{
   return textValue;
+  }
 };
 
 export default selectedTextReducer;
